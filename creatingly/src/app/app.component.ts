4ce9f55131c09fc80700220c1d2d9593d8957587
mohammad7110 <mohammad.abbasi5483@gmail.com>
2024-06-03 13:12:34 +0330

    refactor some code

--- creatingly/src/app/app.component.ts
+++ creatingly/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {Component, ViewChild} from '@angular/core';
-import {Feature, FeatureConfig, FeatureList, getFeatureById} from "../models/features";
-import {DynamicHostDirective} from "../shared/directives/dynamic-host/dynamic-host.directive";
-import {NewFeatureMessage} from "../models/messages/new-feature-message";
-import {MessageService} from "../services/message/message.service";
-import {Message} from "../models/messages/message";
-import {ListSyncMessage} from "../models/messages/list-sync-message";
+import {Component, ComponentRef, ViewChild} from '@angular/core';
+import {Feature, FeatureConfig, FeatureList, getFeatureById} from '../models/features';
+import {DynamicHostDirective} from '../shared/directives/dynamic-host/dynamic-host.directive';
+import {NewFeatureMessage} from '../models/messages/new-feature-message';
+import {MessageService} from '../services/message/message.service';
+import {Message} from '../models/messages/message';
+import {ListSyncMessage} from '../models/messages/list-sync-message';
 
 @Component({
   selector: 'app-root',
@@ -18,58 +18,173 @@ export class AppComponent {
     this.messageService.notification.subscribe(this.receivedNotification.bind(this));
   }
 
-  selectAction(action: FeatureList, isNewFeature: boolean, id?: string, configs?: FeatureConfig, lock?: boolean, data?: any) {
-    const componentRef = this.dynamicHost.viewContainerRef.createComponent(action.component);
-    if (id) {
-      componentRef.instance.id = id;
+  /**
+   * Handles the selection of an action, initializing a new component based on the action provided.
+   * @param action The action that triggers the component creation.
+   * @param isNewFeature Indicates if the action corresponds to a new feature.
+   * @param options its contains instanceId(id property new component ) , configs( detail about position like width , height , top ,left) ,
+   * lock( status of lock property in component when a component lock=true other users couldn't edit it ) data(value of other data of component
+   * for example placeholder ,font-size , color, background-color,...),
+   */
+  selectAction(
+    action: FeatureList,
+    isNewFeature: boolean,
+    options?: {
+      instanceId?: string,
+      configs?: FeatureConfig,
+      lock?: boolean,
+      data?: any
     }
-    if (lock !== undefined) {
-      componentRef.instance.lock = lock;
-    }
-    if (configs) {
-      componentRef.instance.configs = configs;
-    }
-    if (data) {
-      componentRef.instance.formGroup.patchValue(data, {emitEvent: false});
-    }
+  ): void {
+    const componentRef = this.createComponent(action);
 
+    this.initializeComponent(componentRef, options);
+    this.setupComponentRemoval(componentRef);
+    this.handleNewFeature(componentRef, action, isNewFeature);
+  }
+
+  /**
+   * Creates a new component based on the action provided.
+   * @param action The action that determines the component type.
+   * @returns A reference to the newly created component.
+   */
+  private createComponent(action: FeatureList): ComponentRef<any> {
+    return this.dynamicHost.viewContainerRef.createComponent(action.component);
+  }
+
+  /**
+   * Initializes the component with the provided parameters.
+   * @param componentRef Reference to the component to be initialized.
+   * @param options
+   */
+  private initializeComponent(
+    componentRef: ComponentRef<any>,
+    options?: {
+      instanceId?: string,
+      configs?: FeatureConfig,
+      lock?: boolean,
+      data?: any
+    }
+  ): void {
+    if (options) {
+
+      if (options.instanceId) {
+        componentRef.instance.id = options.instanceId
+      }
+      if (options.configs) {
+        componentRef.instance.configs = options.configs;
+      }
+      if (options.lock !== undefined) {
+        componentRef.instance.lock = options.lock;
+      }
+      if (options.data) {
+        componentRef.instance.formGroup.patchValue(options.data, {emitEvent: false});
+      }
+    }
+  }
+
+  /**
+   * Sets up the removal mechanism for the component when the 'remove' event is triggered.
+   * @param componentRef Reference to the component to be removed.
+   */
+  private setupComponentRemoval(componentRef: ComponentRef<any>): void {
     componentRef.instance.remove.subscribe(() => {
       componentRef.destroy();
     });
-    const feature = {
+  }
+
+  /**
+   * Handles the process when a new feature is selected.
+   * @param componentRef Reference to the newly created component.
+   * @param action The action that triggered the component creation.
+   * @param isNewFeature Indicates if the action corresponds to a new feature.
+   */
+  private handleNewFeature(
+    componentRef: ComponentRef<any>,
+    action: FeatureList,
+    isNewFeature: boolean
+  ): void {
+    const feature = this.createFeature(componentRef, action);
+    if (isNewFeature) {
+      this.sendNewFeature(feature);
+    }
+  }
+
+  /**
+   * Creates a feature object based on the component reference and action.
+   * @param componentRef Reference to the component.
+   * @param action The action that triggered the component creation.
+   * @returns The created feature object.
+   */
+  private createFeature(
+    componentRef: ComponentRef<any>,
+    action: FeatureList
+  ): Feature {
+    return {
       componentId: action.componentId,
       instanceId: componentRef.instance.id,
       configs: componentRef.instance.configs,
       lock: componentRef.instance.lock,
       data: componentRef.instance.formGroup.value
-    }
-    if (isNewFeature) {
-      this.sendNewFeature(feature)
-    }
+    };
   }
 
-
+  /**
+   * Handles incoming notifications and processes them accordingly.
+   * @param message The received message.
+   */
   private receivedNotification(message: Message): void {
     if (message instanceof ListSyncMessage) {
-      this.dynamicHost.viewContainerRef.clear();
-      message.list.forEach(item => {
-        const action = getFeatureById(item.componentId);
-        if (action) {
-          this.selectAction(action, false, item.instanceId, item.configs, item.lock, item.data)
-        }
-      })
+      this.syncFeatureList(message.list);
     } else if (message instanceof NewFeatureMessage) {
-      const action = getFeatureById(message.item.componentId);
-      if (action) {
-        this.selectAction(action, false, message.item.instanceId, message.item.configs, message.item.lock, message.item.data)
-      }
+      this.addNewFeature(message.item);
     }
   }
 
-
-  private sendNewFeature(feature: Feature) {
-    const message = new NewFeatureMessage(feature)
-    this.messageService.sendMessage(message)
+  /**
+   * Synchronizes the feature list based on the provided list data.
+   * @param list The list of features to be synchronized it's about detail of components for example instanceId , configs ,  data and lock and
+   * also about componentId in Features.
+   */
+  private syncFeatureList(list: Feature[]): void {
+    this.dynamicHost.viewContainerRef.clear();
+    list.forEach(item => {
+      const action = getFeatureById(item.componentId);
+      if (action) {
+        this.selectAction(action, false, {
+          instanceId: item.instanceId,
+          configs: item.configs,
+          lock: item.lock,
+          data: item.data
+        });
+      }
+    });
   }
 
+  /**
+   * Adds a new feature based on the provided item data.
+   * @param item The item data for the new feature . it's about detail of components for example instanceId , configs ,  data and lock and
+   * also about componentId in Features.
+   */
+  private addNewFeature(item: Feature): void {
+    const action = getFeatureById(item.componentId);
+    if (action) {
+      this.selectAction(action, false,
+        {
+          instanceId: item.instanceId,
+          configs: item.configs,
+          lock: item.lock,
+          data: item.data
+        });
+    }
+  }
+
+  /**
+   * Sends a new feature message through the message service.
+   * @param feature The feature to be sent.
+   */
+  private sendNewFeature(feature: Feature): void {
+    const message = new NewFeatureMessage(feature);
+    this.messageService.sendMessage(message);
+  }
 }
--- creatingly/src/shared/components/feature-generic/feature-generic.component.ts
+++ creatingly/src/shared/components/feature-generic/feature-generic.component.ts
@@ -20,11 +20,8 @@ import {RemoveFeatureMessage} from "../../../models/messages/remove-feature-mess
   styleUrls: ['./feature-generic.component.scss']
 })
 export class FeatureGeneric {
-  // @Output() configChange = new EventEmitter<{ config: FeatureConfig, id: string }>();
-  // @Output() dataChange = new EventEmitter<any>();
 
   @Output() remove = new EventEmitter();
-
 
   formGroup = new FormGroup({
     placeHolder: new FormControl('input'),
@@ -38,42 +35,41 @@ export class FeatureGeneric {
     backgroundColor: new FormControl('rgba(255, 255, 255, 1)'),
   });
 
-
   public id = Utility.generateShortUID();
   public lock: boolean = false;
   public configs: FeatureConfig = {
     x: 0,
     y: 0,
-  }
-
+  };
   private startX = this.configs.x;
   private startY = this.configs.y;
 
   constructor(public renderer: Renderer2, public messageService: MessageService, public dialog: MatDialog) {
     this.messageService.notification.subscribe(this.receivedNotification.bind(this));
+
+
+    // Handle value changes in the form group
     this.formGroup.valueChanges.subscribe((res: any) => {
-      const lockMessage = new LockFeatureMessage(this.id, true);
-      this.messageService.sendMessage(lockMessage);
-      const dataMessage = new DataFeatureMessage(this.id, res);
-      this.messageService.sendMessage(dataMessage)
-    })
-    this.formGroup.valueChanges.pipe(debounceTime(1000)).subscribe(res => {
-      const message = new LockFeatureMessage(this.id, false);
-      this.messageService.sendMessage(message)
-    })
+      this.sendLock(true);
+      this.sendDataMessage(res);
+    });
+
+    // Debounce to avoid frequent updates
+    this.formGroup.valueChanges.pipe(debounceTime(1000)).subscribe(() => {
+      this.sendLock(false);
+    });
   }
 
+
+  /**
+   * Handles incoming messages and updates component state accordingly.
+   * @param message The received message.
+   */
   private receivedNotification(message: Message): void {
     if (message instanceof ConfigFeatureMessage && message.instanceId === this.id) {
       this.configs = message.config;
     } else if (message instanceof LockFeatureMessage && message.instanceId === this.id) {
-      this.lock = message.lock;
-      if (this.lock) {
-        this.formGroup.disable({emitEvent: false})
-      } else {
-        this.formGroup.enable({emitEvent: false});
-      }
-
+      this.handleLockMessage(message);
     } else if (message instanceof DataFeatureMessage && message.instanceId === this.id) {
       this.formGroup.patchValue(message.data, {emitEvent: false});
     } else if (message instanceof RemoveFeatureMessage && message.instanceId === this.id) {
@@ -81,66 +77,93 @@ export class FeatureGeneric {
     }
   }
 
+  /**
+   * Handles drag start events.
+   */
+  dragStart(): void {
+    this.sendDataMessage(true);
+    this.startX = this.configs.x;
+    this.startY = this.configs.y;
+  }
 
+  /**
+   * Handles drag move events.
+   * @param event The drag move event.
+   */
   move(event: CdkDragMove<ElementRef>) {
     const sourcePosition = event.source.getFreeDragPosition();
     this.startX = this.configs.x + sourcePosition.x;
     this.startY = this.configs.y + sourcePosition.y;
-    const message = new ConfigFeatureMessage(this.id, {
+    this.sendConfigMessage({
       width: this.configs.width,
       height: this.configs.height,
       x: this.startX,
       y: this.startY
     });
-    this.messageService.sendMessage(message)
 
   }
 
+
+  /**
+   * Handles drag end events.
+   * @param event The drag end event.
+   */
   drop(event: CdkDragEnd<ElementRef>) {
     const sourcePosition = event.source.getFreeDragPosition();
     this.configs.x += sourcePosition.x;
     this.configs.y += sourcePosition.y;
     this.renderer.setStyle(event.source.element.nativeElement, 'transform', null);
     event.source.reset();
-    const message = new ConfigFeatureMessage(this.id, this.configs);
-    this.messageService.sendMessage(message)
-    const lockMessage = new LockFeatureMessage(this.id, false);
-    this.messageService.sendMessage(lockMessage)
+    this.sendConfigMessage(this.configs);
+    this.sendLock(false);
+
   }
 
 
+  /**
+   * Handles resize start events.
+   */
+  onResizeStart() {
+    this.sendLock(true);
+
+  }
+
+
+  /**
+   * Handles resize move events.
+   * @param event The resize move event.
+   */
   onResizeMove(event: { width: number; height: number; left: number; top: number }) {
     this.configs.width = event.width;
     this.configs.height = event.height;
     this.configs.x = event.left;
     this.configs.y = event.top;
-    const message = new ConfigFeatureMessage(this.id, this.configs);
-    this.messageService.sendMessage(message)
+    this.sendConfigMessage(this.configs);
   }
 
+
+  /**
+   * Handles resize end events.
+   */
   onResizeEnd() {
-    const message = new LockFeatureMessage(this.id, false);
-    this.messageService.sendMessage(message)
+    this.sendLock(false);
   }
 
-  onResizeStart() {
-    const message = new LockFeatureMessage(this.id, true);
-    this.messageService.sendMessage(message)
-  }
 
-  dragStart(): void {
-    const message = new LockFeatureMessage(this.id, true);
-    this.messageService.sendMessage(message)
-    this.startX = this.configs.x;
-    this.startY = this.configs.y;
-  }
-
+  /**
+   * Sends a remove feature message and emits the remove event.
+   */
   removeFeature(): void {
     const message = new RemoveFeatureMessage(this.id);
     this.messageService.sendMessage(message);
     this.remove.emit();
   }
 
+
+  /**
+   * Opens a dialog to change data config if the component is not locked.
+   * @param forms Array of form controls for the dialog.
+   */
   changeDataConfig(forms: Array<{ label: string, control: FormControl }>) {
     if (!this.lock) {
       this.dialog.open(ChangeTextDialogComponent, {
@@ -149,6 +172,48 @@ export class FeatureGeneric {
 
       })
     }
+  }
+
+
+  /**
+   * Sends a lock message to lock or unlock the component.
+   * @param lock Indicates whether to lock or unlock the component.
+   */
+  private sendLock(lock: boolean): void {
+    const lockMessage = new LockFeatureMessage(this.id, lock);
+    this.messageService.sendMessage(lockMessage);
+  }
+
+
+  /**
+   * Sends a data message.
+   * @param data The data to be sent.
+   */
+  private sendDataMessage(data: any): void {
+    const dataMessage = new DataFeatureMessage(this.id, data);
+    this.messageService.sendMessage(dataMessage);
+  }
+
+
+  /**
+   * Handles lock messages.
+   * @param message The lock message.
+   */
+  private handleLockMessage(message: LockFeatureMessage): void {
+    this.lock = message.lock;
+    if (this.lock) {
+      this.formGroup.disable({emitEvent: false});
+    } else {
+      this.formGroup.enable({emitEvent: false});
+    }
+  }
+
+  /**
+   * Sends a configuration message.
+   */
+  private sendConfigMessage(config: FeatureConfig): void {
+    const message = new ConfigFeatureMessage(this.id, config);
+    this.messageService.sendMessage(message);
   }
 
 
